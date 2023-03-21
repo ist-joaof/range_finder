87278e5a12f89508c850a90423d5d31db8a81889
ist-joaof <joao.fonseca@ist.utl.pt>
2023-03-21 12:02:43 +0000

    Update app.py
    juniper dump rounting parser correction

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def range_search(ip, filename, effectiveroutes):
         for line in lines:
             aux = line.split()
             if len(aux) > 0:
-                if aux[0] == 'B' or aux[0] == 'L' or aux[0] == 'C' or ('/' in aux[0] and aux[0] != 'I/O'):
+                if aux[0] == 'B' or aux[0] == 'L' or aux[0] == 'C' or ('/' in aux[0] and aux[0] != 'I/O' and 'BGP' not in aux[0]):
                     if '/' not in aux[0]:
                         net = ip_network(aux[1], strict=False)
                     else:
